239d9682b69c535cda856d16dd5f04c2d1895fbb
Endy <endy.alexis@outlook.com>
2023-01-19 02:08:29 +0100

    Create home componenents
    Added:
    - Home grid layouts components
    - Image assets
    
    Updated:
    - Global css style
    - responsive

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 // import { useLocation, useNavigate } from 'react-router'
 import Banner from '../home/Banner'
 import { motion } from 'framer-motion'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     // const location = useLocation()
@@ -14,9 +15,67 @@ const Home = () => {
     return (
         <>
             <motion.div className='wrapper-container' initial={{opacity: 0}} animate={{opacity:1}} exit={{opacity: 0}} >
+                <Banner />
                 <div className='container'>
-                    <Banner />
-                    Yo man 
+                    <div className='gutter'>
+                        <div className='grid-2 pad-t-xl pad-b-xl'>
+                            <div className='grid-item'>
+                                <div className='card-wrapper marg-b-xl'>
+                                    <div className='card-display'>
+                                        <img src={require('../../images/burgercamere.jpg')} alt='Burger de ouf' />
+                                    </div>
+                                    <div className='card-display-filter'></div>
+                                </div>
+                            </div>
+                            <div className='grid-item'>
+                                <div className='flex-col-start'>
+                                    <div className='flex-col mid-container'>
+                                        <h2 className='large-text bold-text violet-text pad-b-m'>Que proposons-nous ?</h2>
+                                        <p className='medium-text regular-text black-text pad-b-l'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae dapibus nibh. Fusce blandit lorem quis mattis ullamcorper. Vivamus maximus convallis aliquet. Pellentesque non aliquam dolor, nec efficitur velit. Vestibulum ac hendrerit sapien, vulputate ultricies nibh. Curabitur ut nisi euismod, tristique leo sit amet, convallis justo.</p>
+                                        <Link className='btn btn-bordered btn-medium btn-swift' to='/'>Consulter le menu</Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='grid-2-r pad-t-xl pad-b-xl'>
+                            <div className='grid-item'>
+                                <div className='flex-col-end'>
+                                    <div className='flex-col mid-container'>
+                                        <h2 className='large-text bold-text sand-text pad-b-m'>Qui sommes-nous ?</h2>
+                                        <p className='medium-text regular-text black-text pad-b-l'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae dapibus nibh. Fusce blandit lorem quis mattis ullamcorper. Vivamus maximus convallis aliquet. Pellentesque non aliquam dolor, nec efficitur velit. Vestibulum ac hendrerit sapien, vulputate ultricies nibh. Curabitur ut nisi euismod, tristique leo sit amet, convallis justo.</p>
+                                        <Link className='btn btn-bordered btn-medium btn-cream' to='/'>Contacter-nous</Link>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='grid-item'>
+                                <div className='card-wrapper marg-b-xl'>
+                                    <div className='card-display'>
+                                        <img src={require('../../images/auth_page.jpg')} alt='Burger de ouf' />
+                                    </div>
+                                    <div className='card-display-filter'></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='grid-2 pad-t-xl pad-b-xl'>
+                            <div className='grid-item'>
+                                <div className='card-wrapper marg-b-xl'>
+                                    <div className='card-display'>
+                                        <img src={require('../../images/presentation.jpg')} alt='Burger de ouf' />
+                                    </div>
+                                    <div className='card-display-filter'></div>
+                                </div>
+                            </div>
+                            <div className='grid-item'>
+                                <div className='flex-col-start'>
+                                    <div className='flex-col mid-container'>
+                                        <h2 className='large-text bold-text red-text pad-b-m'>Où nous trouver ?</h2>
+                                        <p className='medium-text regular-text black-text pad-b-l'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae dapibus nibh. Fusce blandit lorem quis mattis ullamcorper. Vivamus maximus convallis aliquet. Pellentesque non aliquam dolor, nec efficitur velit. Vestibulum ac hendrerit sapien, vulputate ultricies nibh. Curabitur ut nisi euismod, tristique leo sit amet, convallis justo.</p>
+                                        <Link className='btn btn-bordered btn-medium btn-danger' to='/'>Passer commande</Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </motion.div>
         </>
